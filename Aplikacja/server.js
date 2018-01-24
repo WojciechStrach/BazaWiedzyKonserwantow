e08fdc7e1dd97d20b7171e5763bdde0693bd00e6
Wojciech <wojciech.strach1@gmail.com>
2018-01-24 14:12:06 +0100

    PreservativeSearch

--- Aplikacja/server.js
+++ Aplikacja/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const neo4j = require('neo4j');
 const db = new neo4j.GraphDatabase('http://neo4j:baza@localhost:7474');
 const productModel = require('./models/product');
+const preservativeModel = require('./models/preservative');
 
 const app = express();
 
@@ -11,7 +12,7 @@ const app = express();
 app.use(bodyParser.json())
 const urlencodedParser = bodyParser.json({ extended: false });
 
-
+//routes
 app.get('/', function(req, res){
     res.status(200).send('<h1>Knowledge database API</h1>');
 });
@@ -49,7 +50,7 @@ app.post('/search/product', urlencodedParser, function(req,res){
 
             var result = results[0];
             if (!result) {
-                res.status(204);
+                res.status(204).send();
             } else {
 
                 product.setProductName(results[0].x.properties.Nazwa);
@@ -57,7 +58,7 @@ app.post('/search/product', urlencodedParser, function(req,res){
 
                 db.cypher({
                     query: 'MATCH (x:Nazwa {Nazwa: {preservativesSearch}})' +
-                           'OPTIONAL MATCH (x)-[r:Zawiera]->(y)' +
+                           'MATCH (x)-[r:Zawiera]->(y)' +
                            'RETURN y',
                     params: { 
                         preservativesSearch: results[0].x.properties.Nazwa,
@@ -71,7 +72,9 @@ app.post('/search/product', urlencodedParser, function(req,res){
         
                     var preservativesResult = preservativesResults[0];
                     if (!preservativesResult) {
-                        
+
+                        res.status(200).send('<h4>Product does not contain any preservatives</h4>');
+                    
                     } else {
 
                         let preservativesTempObject = [];
@@ -229,6 +232,179 @@ app.post('/search/product', urlencodedParser, function(req,res){
 });
 
 app.post('/search/preservative', urlencodedParser, function(req,res){
+
+    var body = req.body;
+    var searchValue;
+
+    if (typeof body.search === "undefined"){
+
+        let jsonString = JSON.stringify({"search":"query_that_you_want_to_search"});
+        let response = 'JSON data are not valid, please provide data in ' + jsonString + ' format';
+
+        res.status(400).send('<h4>' + response + '</h4>');
+
+    }else{
+
+        searchValue = body.search;
+
+        var preservative = new preservativeModel.PreservativeModel();
+
+        db.cypher({
+            query: 'MATCH (x:Oznaczenie {Oznaczenie: {preservativeSearch}})' +
+                   'RETURN x',
+            params: { 
+                preservativeSearch: searchValue,
+            },
+        }, function (err, results) {
+
+            if (err) {
+                console.log(err);
+                res.status(400).send('<h4>Unexpecting error occured ' + err + '</h4>');
+            }
+
+            var result = results[0];
+            if (!result) {
+                res.status(204).send();
+            } else {
+                preservative.setPreservativeSign(results[0].x.properties.Oznaczenie);
+                preservative.setPreservativeCommonName(results[0].x.properties.Nazwa_zwyczajowa);
+                preservative.setPreservativeDescribe(results[0].x.properties.Opis);
+
+                db.cypher({
+                    query: 'MATCH (x:Oznaczenie {Oznaczenie: {typeSearch}})' +
+                           'MATCH (x)-[:Jest_instancją]->(y)' +
+                           'RETURN y',
+                    params: { 
+                        typeSearch: results[0].x.properties.Oznaczenie,
+                    },
+                }, function (typeSearchErr, typeSearchResults) {
+        
+                    if (typeSearchErr) {
+                        console.log(typeSearchErr);
+                        res.status(400).send('<h4>Unexpecting error occured ' + typeSearchErr + '</h4>');
+                    }
+        
+                    var typeSearchResult = typeSearchResults[0];
+                    if (!typeSearchResult) {
+                        
+                    } else {
+                        preservative.setPreservativeType(typeSearchResults[0].y.properties.Typ_dodatku_do_żywności);
+
+                        db.cypher({
+                            query: 'MATCH (x:Oznaczenie {Oznaczenie: {diseasesSearch}})' +
+                                   'MATCH (x)-[:Może_powodować]->(y)' +
+                                   'RETURN y',
+                            params: { 
+                                diseasesSearch: results[0].x.properties.Oznaczenie,
+                            },
+                        }, function (diseasesSearchErr, diseasesSearchResults) {
+                
+                            if (diseasesSearchErr) {
+                                console.log(diseasesSearchErr);
+                                res.status(400).send('<h4>Unexpecting error occured ' + diseasesSearchErr + '</h4>');
+                            }
+                
+                            var diseasesSearchResult = diseasesSearchResults[0];
+                            console.log(diseasesSearchResult);
+                            if (!diseasesSearchResult) {
+                                let emptyTab = [];
+                                preservative.setPreservativeDiseases(emptyTab);
+
+                                db.cypher({
+                                    query: 'MATCH (x:Oznaczenie {Oznaczenie: {productsSearch}})' +
+                                           'MATCH (x)<-[:Zawiera]-(y)' +
+                                           'RETURN y',
+                                    params: { 
+                                        productsSearch: results[0].x.properties.Oznaczenie,
+                                    },
+                                }, function (productsSearchErr, productsSearchResults) {
+                        
+                                    if (productsSearchErr) {
+                                        console.log(productsSearchErr);
+                                        res.status(400).send('<h4>Unexpecting error occured ' + productsSearchErr + '</h4>');
+                                    }
+                        
+                                    var productsSearchresult = productsSearchResults[0];
+                                    if (!productsSearchresult) {
+                                        let emptyProductsTab = [];
+                                        preservative.setPreservativeProducts(emptyProductsTab);
+
+                                        res.status(200).json(preservative);
+                                    } else {
+                                        let productsTempTab = [];
+                                        for(let i=0; i<productsSearchResults.length; i++){
+                                            
+                                            productsTempTab.push(productsSearchResults[i].y.properties.Nazwa);        
+                                            
+                                        }
+
+                                        preservative.setPreservativeProducts(productsTempTab);
+
+                                        res.status(200).json(preservative);
+
+                                    }
+                                });
+
+                                
+                            } else {
+                                let diseasesTempTab = [];
+
+                                for(let i=0; i<diseasesSearchResults.length; i++){
+                                    for(let disease in diseasesSearchResults[i].y.properties){
+                                        diseasesTempTab.push(diseasesSearchResults[i].y.properties[disease]);        
+                                    }
+                                }
+
+                                preservative.setPreservativeDiseases(diseasesTempTab);
+
+                                db.cypher({
+                                    query: 'MATCH (x:Oznaczenie {Oznaczenie: {productsSearch}})' +
+                                           'MATCH (x)<-[:Zawiera]-(y)' +
+                                           'RETURN y',
+                                    params: { 
+                                        productsSearch: results[0].x.properties.Oznaczenie,
+                                    },
+                                }, function (productsSearchErr, productsSearchResults) {
+                        
+                                    if (productsSearchErr) {
+                                        console.log(productsSearchErr);
+                                        res.status(400).send('<h4>Unexpecting error occured ' + productsSearchErr + '</h4>');
+                                    }
+                        
+                                    var productsSearchresult = productsSearchResults[0];
+                                    if (!productsSearchresult) {
+                                        let emptyProductsTab = [];
+                                        preservative.setPreservativeProducts(emptyProductsTab);
+
+                                        res.status(200).json(preservative);
+                                    } else {
+                                        let productsTempTab = [];
+                                        for(let i=0; i<productsSearchResults.length; i++){
+        
+                                            productsTempTab.push(productsSearchResults[i].y.properties.Nazwa);        
+                                            
+                                        }
+
+                                        preservative.setPreservativeProducts(productsTempTab);
+
+                                        res.status(200).json(preservative);
+
+                                    }
+                                });
+                                
+
+                            }
+                        });
+
+                    }
+                });
+
+                
+            }
+        });
+
+
+    }
 
 });
 
